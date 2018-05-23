d40a3b87f3c7dad21557184637fd0f95ca1ab3b9
Raul Escamilla <raul.escamilla@convergys.com>
2018-05-23 10:49:37 -0600

    About library added

---
+++ spacyemoticon/about.py
@@ -0,0 +1,7 @@
+__title__ = 'spacyemoticon'
+__version__ = '1.0.0'
+__summary__ = 'spaCy pipeline component for adding emoticon meta data to Doc, Token and Span objects that are text emoticons.'
+__uri__ = 'https://spacy.io'
+__author__ = 'Raúl Escamilla'
+__email__ = 'raul.escamilla@asesoriait.com'
+__license__ = 'MIT'
